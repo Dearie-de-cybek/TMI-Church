9948b1c78189acc54a37bed0aac633d7a30af04b
Dearie <eburujacintha@gmail.com>
2025-05-05 00:41:04 +0400

    made setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <h1 className='text-amber-400'>Twain man Ministries</h1>
+      <h1 className='text-amber-400 text-5xl'>Twain man Ministries</h1>
     </>
   )
 }
